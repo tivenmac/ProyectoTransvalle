de8e3d4cd78f8fb21f57ea2bfa791aec9800583f
OscarV <ing.odvega@gmail.com>
2017-01-12 15:22:05 -0500

    otro commit

--- src/lol/Lol.java
+++ src/lol/Lol.java
@@ -16,6 +16,8 @@ public class Lol {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.err.println("hola estibenson");
     }
     
 }
